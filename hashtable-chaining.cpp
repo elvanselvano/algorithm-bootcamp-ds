8581ca71f32e6cbdd024de79762d2376ca317c2a
Elvan Selvano <elvanselvano@yahoo.com>
2020-12-24 22:37:14 +0700

    create traverseLinkedList() for modularity

--- hashtable-chaining.cpp
+++ hashtable-chaining.cpp
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+// max_element bukan menandakan panjang node
+// dari sebuah linked list akan tetapi jumlah linked list 
+// maksimal dalam sebuah hashtable
 const int MAX_ELEMENT = 10;
 
 struct Node {
   char name[255];
   Node *next;
+  // terdapat 10 buah linked list dengan 10 head dan tail
 } *head[MAX_ELEMENT], *tail[MAX_ELEMENT];
 
 Node *createNode(const char *name) {
@@ -27,28 +31,37 @@ unsigned long djb2(const char *str) {
 }
 
 void insert(const char *str) {
+  // 0 -> .. -> ..
+  // 1 -> .. 
+  // 2
+  // 3 -> .. -> .. -> ..
+  Node *temp = createNode(str);
   int index = djb2(str);
 
-  if(head[index]) { // cek apakah dia sudah terisi
-    Node *temp = createNode(str);
+  if(!head[index]) { // cek apakah headnya kosong
+    head[index] = tail[index] = temp;
+  } else { // jika sudah terisi lakukan push tail
     tail[index]->next = temp;
     tail[index] = temp;
-  } else { // cek apakah dia belum terisi
-    head[index] = tail[index] = createNode(str);
+  }
+}
+
+void traverseLinkedList(int i) {
+  Node *curr = head[i];
+
+  while(curr) {
+    printf("%s -> ", curr->name);
+    curr = curr->next;
   }
 }
 
 void view() {
   for(int i = 0; i < MAX_ELEMENT; i++) {
-    printf("%d ", i);
-    if(head[i]) {
-      Node *curr = head[i];
-      while(curr) {
-        printf("%s -> ", curr->name);
-        curr = curr->next;
-      }
-    }
-    puts("");
+    printf("Index %d: ", i);
+    if(head[i]) { // jika ada head
+      traverseLinkedList(i); // kita traverse
+    } 
+    puts("NULL");
   }
 }
 
@@ -58,6 +71,8 @@ int main() {
   insert("kesya");
   insert("nich");
   insert("valencia");
+  insert("laffpai");
+  insert("kevice");
   view();
   return 0;
 }
