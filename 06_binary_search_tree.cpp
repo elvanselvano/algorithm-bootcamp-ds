8a428c65b34ee3a304b993508a6ea1a95e9953f8
Elvan Selvano <elvanselvano@yahoo.com>
2020-12-28 22:00:10 +0700

    updated deleteNode and minValueNode

--- 06_binary_search_tree.cpp
+++ 06_binary_search_tree.cpp
@@ -14,91 +14,73 @@ Node *createNode(int value) {
 }
 
 Node *insertNode(Node *root, int value) {
-  if (!root) { // jika tidak ada root
-    return createNode(value);
+  if(!root) { // root sekarang masih NULL
+    return createNode(value); // buat nodenya
   } else if(value < root->value) { // masuk subtree kiri
-    root->left = insertNode(root->left, value); // traverse ke subtree kiri
+    root->left = insertNode(root->left, value);
   } else if(value > root->value) { // masuk subtree kanan
-    root->right = insertNode(root->right, value); // traverse ke subtree kanan
+    root->right = insertNode(root->right, value);
   }
 
   return root;
 }
 
-// mencari minimum value dalam sebuah subtree
-Node *minValueNode(Node *root) {
-  Node *curr = root;
+// mencari nilai terkecil di subtree kanan
+Node *minValueNode(Node *root) { 
+  Node *curr = root->right; // curr adalah parent di subtree kanan
 
-  // traverse terus ke kiri sampai sukses
-  while(curr && curr->left) {
-    curr = curr->left;
+  while(curr && curr->left) { 
+    curr = curr->left; // traverse terus menerus ke kiri
   }
 
   return curr;
 }
 
 Node *deleteNode(Node *root, int value) {
-  if(!root) { // yg kita mw delete tidak ada
+  if(!root) { // yang mau didelete tidak ada
     return root;
-  } else if(value < root->value) { // traverse kiri
-    // sambil initialize link karna nanti bakal ada perubahan link
+  } else if(value < root->value) { // masuk subtree kiri
     root->left = deleteNode(root->left, value);
-  } else if(value > root->value) { // traverse kanan
-    // sambil initialize link karna nanti bakal ada perubahan link
+  } else if(value > root->value) { // masuk subtree kanan
     root->right = deleteNode(root->right, value);
-  } else if(value == root->value) { // ketemu apa yang mau kita delete
-
-    // kasus 0 anak dan 1 anak
-    if(!root->left) { // jika kiri null, kandidat kanan
-      Node *temp = root->right; 
-      free(root);
-      return temp;
-    } else if(!root->right) { // jika kanan null, kandidat kiri
-      Node *temp = root->left; // kandidat pengganti anak kiri
-      free(root); // hapus rootnya
-      return temp; // return anak kiri
+  } else { // ketemu yang mau dihapus
+    if(!root->left || !root->right) { // kasus 0 dan 1 anak
+      // jika kiri null, kandidatnya kanan dan sebaliknya
+      Node *newRoot = root->left ? root->left : root->right;
+      free(root); // hapus root lama
+      root = NULL;
+      return newRoot;
     }
 
-    // Initial Tree
-    //    10
-    //  4   12  
-
-    // delete 10
-    // temp = 12
-    // root->value = temp->value
-
-    // After Delete
-    //    12
-    //  4    
-
-    // cari temp->value di subtree kanan
-    // root->right = deleteNode(root->right, temp->value);
-
-    // jika kedua if di atas tidak dijalankan, artinya dia masuk kasus 2 anak
-
-    // cari nilai terkecil di subtree kanan (pembuktian tonton di recording)
-    Node *temp = minValueNode(root->right);
-    root->value = temp->value; // copy valuenya dulu sebelum dihapus
-    // ketika kita ingin hapus sebuah node 2 anak, bukan node itu yg dihapus tapi penggantinya
-    root->right = deleteNode(root->right, temp->value); // bakal hapus nilai dari node terkecil di subtree kanan (hapus penggantinya)
+    // kasus 2 anak
+    Node *inOrderSucessor = minValueNode(root); // nilai terkecil di subtree kanan
+    root->value = inOrderSucessor->value; // copy valuenya
+    root->right = deleteNode(root->right, inOrderSucessor->value); // hapus node pengganti
   }
 
   return root;
 }
 
-// tidak perlu initialize link, cukup rekursi
+//                  15
+//          10              23
+//     8         12     18       27
+// 15->left = insertNode(15->left, 13);
+// 10->right = insertNode(10->right, 13);
+// 12->right = insertNode(12->right, 13);
+// if(!root), createNode(13)
+
 bool searchNode(Node *root, int key) {
-  if(root) { // selama root bukan null
-    if(root->value == key) { // jika nilai root adalah yang dicari
+  if(root) {
+    if(root->value == key) { // jika ketemu apa yang dicari
       return true;
-    } else if(key < root->value) { // masuk subtree kiri
-      return searchNode(root->left, key);
-    } else if(key > root->value) { // masuk subtree kanan
-      return searchNode(root->right, key);
+    } else if(key < root->value) {
+      return searchNode(root->left, key); // cari di subtree kiri
+    } else if(key > root->value) {
+      return searchNode(root->right, key); // cari di subtree kanan
     }
   }
 
-  return false;
+  return false; // engga ketemu
 }
 
 void preOrder(Node *root) {
@@ -106,6 +88,14 @@ void preOrder(Node *root) {
     printf("%d ", root->value);
     preOrder(root->left);
     preOrder(root->right);
+  }
+}
+
+void inOrder(Node *root) { // sorted
+  if(root) {
+    inOrder(root->left);
+    printf("%d ", root->value);
+    inOrder(root->right);
   }
 }
 
@@ -117,28 +107,17 @@ void postOrder(Node *root) {
   }
 }
 
-void printAll(Node *root) { // inorder: print sorted
-  if(root) {
-    printAll(root->left);
-    printf("%d ", root->value);
-    printAll(root->right);
-  }
-}
-
 int main() {
-  Node *baseRoot = NULL; // init null
-  baseRoot = insertNode(baseRoot, 15);
-  baseRoot = insertNode(baseRoot, 20); // add new item
-  baseRoot = insertNode(baseRoot, 10); // ...
+  Node *baseRoot = NULL;
+  baseRoot = insertNode(baseRoot, 10);
+  baseRoot = insertNode(baseRoot, 5);
+  baseRoot = insertNode(baseRoot, 13);
+  baseRoot = insertNode(baseRoot, 16);
+  baseRoot = insertNode(baseRoot, 21);
+  baseRoot = insertNode(baseRoot, 4);
   baseRoot = insertNode(baseRoot, 7);
-  printAll(baseRoot); puts("");
-  baseRoot = deleteNode(baseRoot, 7);
-  printAll(baseRoot); puts("");
   baseRoot = deleteNode(baseRoot, 10);
-  printAll(baseRoot); puts("");
-  baseRoot = deleteNode(baseRoot, 15);
-  printAll(baseRoot); puts("");
-  baseRoot = deleteNode(baseRoot, 20);
-  printAll(baseRoot); puts("");
+  inOrder(baseRoot);
+
   return 0;
 }
