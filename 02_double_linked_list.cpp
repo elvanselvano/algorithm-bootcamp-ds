88f0083de31b7d23c0341f17398c8befd4cf05e7
Elvan Selvano <elvanselvano@yahoo.com>
2021-01-14 07:00:33 +0700

    added visualization

--- 02_double_linked_list.cpp
+++ 02_double_linked_list.cpp
@@ -3,22 +3,22 @@
 
 struct Node {
   int value;
-  Node *next, *prev;
-} *head, *tail, *curr;
+  Node *next, *prev; // point to the left and right
+} *head, *tail; // point to the leftmost and rightmost node
 
 Node *createNode(int value) {
-  Node *temp = (Node*)malloc(sizeof(Node));
-  temp->value = value;
-  temp->next = temp->prev = NULL;
-  return temp;
+  Node *newNode = (Node*)malloc(sizeof(Node));
+  newNode->value = value;
+  newNode->next = newNode->prev = NULL;
+  return newNode;
 }
 
 void pushHead(int value) {
   Node *temp = createNode(value);
 
-  if(!head) { // 0 node
-    head = tail = temp; // Linked List: temp (head dan tail)
-  } else { // Linked List: A (head), mau insert B
+  if(!head) { // empty list
+    head = tail = temp; // temp (head and tail)
+  } else { // A (head), insert B
     head->prev = temp; // B (temp) <- A (head)
     temp->next = head; // B (temp) -><- A (head)
     head = temp; // B (temp, head) -><- A
@@ -28,9 +28,9 @@ void pushHead(int value) {
 void pushTail(int value) {
   Node *temp = createNode(value);
 
-  if(!head) { // 0 node
-    head = tail = temp; // Linked List: temp (head dan tail)
-  } else { // Linked List: A (tail), mau insert B
+  if(!head) { // empty list
+    head = tail = temp; // temp (head dan tail)
+  } else { // A (tail), insert B
     tail->next = temp; // A (tail) -> B (temp)
     temp->prev = tail; // A (tail) -><- B (temp)
     tail = temp;  // A -><- B (temp, tail)
@@ -38,82 +38,83 @@ void pushTail(int value) {
 }
 
 void pushMid(int value) {
-  if(!head) { // tidak ada node
+  if(!head) { // empty list
     Node *temp = createNode(value);
     head = tail = temp;
-  } else if(value < head->value) { // nilai kurang dari head
+  } else if(value < head->value) { // add to the leftmost
     pushHead(value);
-  } else if(value > tail->value) { // nilai lebih dari tail
+  } else if(value > tail->value) { // add to the rightmost
     pushTail(value);
-  } else { // nilai di antara head dan tail
-    // 1 (curr) -> 8 -> 12 -> 15, mau insert 9
-    Node *temp = createNode(value);
-    Node *curr = head;
+  } else { // value is between head and tail
+    Node *temp = createNode(value); // 9 (temp)
+    Node *curr = head; // 1 (head, curr) -><- 8 -><- 12 -><- 15, insert 9
 
-    // selama nilainya kurang dari value
     while(curr->value < value) {
-      curr = curr->next;
+      curr = curr->next; // traverse until curr->value > value
     }
-    // 1 -><- 8 -><- 12 (curr) -><- 15, mau insert 9
-    // 1 -><- 8 -><- 9 -><- 12 (curr) -><- 15
 
-    temp->prev = curr->prev; // temp->prev = 8
-    temp->next = curr; // temp->next = 12
-    // Hasilnya: 1 -><- 8 ->      <- 12 (curr) -><- 15
-    //                   \(prev)   /(next)
-    //                         temp
+    // 1 -><- 8 -><- 12 (curr) -><- 15 || 9 (temp)
+    temp->prev = curr->prev; // 9->prev = 8
+    temp->next = curr; // 9->next = 12
+    
+    // 1 -><- 8 ->      <- 12 (curr) -><- 15
+    //        \(prev)   /(next)
+    //               9 (temp)
 
-    curr->prev->next = temp; // 8->next = temp
-    curr->prev = temp; // 12->prev = temp
-    // Hasilnya: 1 -><- 8                           12 (curr) -><- 15
-    //                   \(prev)\(next)     /(next)/(prev)
-    //                                 temp
+    curr->prev->next = temp; // 8->next = 9
+    curr->prev = temp; // 12->prev = 9
+    // 1 -><- 8                           12 (curr) -><- 15
+    //       \(prev)\(next)     /(next)/(prev)
+    //                   9 (temp)
   }
 }
 
 void popHead() {
-  if(!head) { // 0 node
+  if(!head) { // empty list
     return;
   } else if(head == tail) { // 1 node
     free(head);
     head = tail = NULL;
   } else { // >= 1 node
-    Node *temp = head->next; // A (head) -><- B (temp) -><- C
-    head->next = temp->prev = NULL; // A (head) B (temp) -><- C
+    Node *newHead = head->next; // A (head) -><- B (temp) -><- C
+    head->next = newHead->prev = NULL; // A (head) || B (temp) -><- C
     free(head); // B (temp) -><- C
-    head = temp; // B (head, temp) -><- C
+    head = newHead; // B (head, temp) -><- C
   }
 }
 
 void popTail() {
-  if(head && head == tail) { // jika hanya 1 node
-    head = tail = NULL;
+  if(!head) { // empty list
+    return; 
+  } else if(head == tail) { // 1 node
     free(head);
-  } else { // jika > 1 node // A -><- B -><- C (tail)
+    head = tail = NULL;
+  } else { // >= 1 node
     Node *newTail = tail->prev; // A -><- B (newTail) -><- C (tail)
-    tail->prev = newTail->next = NULL; // A -><- B (newTail) C (tail)
+    tail->prev = newTail->next = NULL; // A -><- B (newTail) || C (tail)
     free(tail); // A -><- B (newTail)
-    tail = newTail; // A -><- B (tail)
+    tail = newTail; // A -><- B (tail, newTail)
   }
 }
 
 void popMid(int value) {
-  if(!head) {
+  if(!head) { // empty list
     return;
   } else if(head->value == value) {
-    popHead(); // jika nilai head adalah yang mau kita hapus
+    popHead(); 
   } else if(tail->value == value) {
-    popTail(); // jika nilai tail adalah yang mau kita hapus
-  } else { // jika nilai yang mau kita hapus ada di tengah2
-    curr = head; // 10 (curr) 20 30 40 50, mau hapus 30
+    popTail(); 
+  } else { // value is between head and tail
+    Node *curr = head; // 10 (curr) 20 30 40 50, remove 30
 
     while(curr && curr->value != value) {
-      curr = curr->next;
+      curr = curr->next; // traverse until we find the value
     }
-    // 10 -><- 20 -><- 30 (curr) -><- 40 -><- 50, mau hapus 30
 
+    // 10 -><- 20 -><- 30 (curr) -><- 40 -><- 50, remove 30
     curr->prev->next = curr->next; // 20->next = 40
     curr->next->prev = curr->prev; // 40->prev = 20
+
     // 10 -><- 20      -><-     40 -><- 50, mau hapus 30
     //          \<- 30 (curr) ->/ 
 
@@ -123,43 +124,37 @@ void popMid(int value) {
   }
 }
 
-bool search(int value) {
-  curr = head;
+void printLinkedList() {
+  Node *curr = head;
+  printf("Linked List: ");
 
   while(curr) {
-    if(curr->value == value) {
-      return true;
-    }
+    printf("%d -> ", curr->value);
     curr = curr->next;
   }
-
-  return false;
+  puts("NULL");
 }
 
-void printLinkedList() {
-  curr = head;
-
-  while(curr) {
-    printf("%d ", curr->value);
-    curr = curr->next;
+void visualizeList(int N, int values[]) {
+  for(int i = 0; i < N; i++) {
+    pushMid(values[i]);
+    printf("Inserted %d to the list.\n", values[i]);
+    printLinkedList();
+    getchar();
   }
-  puts("");
+
+  for(int i = 0; i < N; i++) {
+    popMid(values[i]);
+    printf("Removed %d from the list.\n", values[i]);
+    printLinkedList();
+    getchar();
+  }
 }
 
 int main() {
-  pushMid(15);
-  pushMid(10);
-  pushMid(20);
-  pushMid(25);
-  printLinkedList();
-  popHead();
-  printLinkedList();
-  popMid(20);
-  printLinkedList();
-  popMid(15);
-  printLinkedList();
-  popMid(25);
-  printLinkedList();
+  int N = 8;
+  int values[] = {10, 9, 13, 12, 5, 11, 8, 15};
+  visualizeList(N, values);
 
   return 0;
 }
