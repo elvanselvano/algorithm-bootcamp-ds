1f4186a0f21aba9fc38478cac88e86f77c06f2b9
Elvan Selvano <elvanselvano@yahoo.com>
2021-01-14 07:10:02 +0700

    add visualization

--- 01_single_linked_list.cpp
+++ 01_single_linked_list.cpp
@@ -28,55 +28,47 @@ void pushHead(const char *name, int score) {
 }
 
 void pushTail(const char *name, int score) {
-  // Kita mau pushTail D10 di linked list berikut:
-  // A2 (head) -> B3 -> C4 (tail) 
+  Node *temp = createNode(name, score);
 
-  // D10 (temp) -> NULL || Node *temp = createNode(name, score)
-  // A2 (head) -> B3 -> C4 (tail) -> D10 (temp) -> NULL || tail->next = temp
-  // A2 (head) -> B3 -> C4 -> D10 (tail) -> NULL || tail = temp
-
-  Node *temp = createNode(name, score); // create new node
-
-  if(!head) { // if there's no head (0 node)
-    head = tail = temp;
-  } else { // >= 1 node
-    tail->next = temp; // tail points to node
-    tail = temp; // node becomes tail
+  if(!head) { // empty list
+    head = tail = temp; // temp (head dan tail)
+  } else { // A (tail), insert B
+    tail->next = temp; // A (tail) -> B (temp)
+    tail = temp; // A -> B (tail, temp)
   }
 }
 
 void popHead() {
-  if(!head) { // 0 node
+  if(!head) { // empty list
     return;
-  } else if(head == tail) { // only 1 node
+  } else if(head == tail) { // 1 node
     free(head); // free allocated memory
     head = tail = NULL; // remove value
   } else { // >= 1 node
-    Node *temp = head->next; // 98 (head) -> 100 (temp) -> 95 -> 97 (tail) -> NULL
-    head->next = NULL; // 98 (head) -> N || 100 (temp) -> 95 -> 97 (tail) -> NULL
+    Node *temp = head->next; // A (head) -> B (newHead) -> C
+    head->next = NULL; // A (head) || B (newHead) -> C
     free(head); // free allocated memory
-    head = temp; // set new head
+    head = temp; // B (head, newHead) -> C
   }
 }
 
 void popTail() {
-  if(!head) { // 0 node
+  if(!head) { // empty list
     return;
-  } else if(head == tail) { // only 1 node
+  } else if(head == tail) { // 1 node
     free(head); // free allocated memory
     head = tail = NULL; // remove value
   } else {
-    // INITIAL: 50 (head) -> 54 -> 73 -> 36 (tail) -> NULL
-    Node *temp = head; // 50 (head, temp) -> 54 -> 73 -> 36 (tail) -> NULL
+    Node *curr = head; // 50 (head, curr) -> 54 -> 36 (tail) -> NULL
 
-    while(temp->next != tail) { // selama node selanjutnya bukan tail
-      temp = temp->next; // loop terus
+    while(curr->next != tail) {
+      curr = curr->next; // loop until one node behind tail
     }
 
-    // 50 (head) -> 54 -> 73 (temp) -> 36 (tail) -> NULL
-    temp->next = NULL; // 50 (head) -> 54 -> 73 (temp) -> NULL || 36 (tail) -> NULL
-    free(tail); // 50 (head) -> 54 -> 73 (temp) -> NULL
-    tail = temp; // 50 (head) -> 54 -> 73 (tail, temp) -> NULL
+    // 50 (head) -> 54 (curr) -> 36 (tail) -> NULL
+    curr->next = NULL; // 50 (head) -> 54 (curr) -> NULL || 36 (tail) -> NULL
+    free(tail); // 50 (head) -> 54 (curr) -> NULL
+    tail = curr; // 50 (head) -> 54 (tail, curr) -> NULL
   }
 }
 
@@ -88,16 +80,24 @@ void printLinkedList() {
     curr = curr->next; // move to the next node
   }
   puts("NULL");
+  getchar();
 }
 
 int main() {
   pushHead("Darnell", 95); // 95 (head, tail) -> NULL
+  printLinkedList();
   pushTail("whisper0", 97); // 95 (head) -> 97 (tail) -> NULL
+  printLinkedList();
   pushHead("pai", 100); // 100 (head) -> 95 -> 97 (tail) -> NULL
+  printLinkedList();
   pushHead("romario", 98); // 98 (head) -> 100 -> 95 -> 97 (tail) -> NULL
+  printLinkedList();
   popHead(); // 100 (head) -> 95 -> 97 (tail) -> NULL
+  printLinkedList();
   popTail(); // 100 (head) -> 95 (tail) -> NULL
+  printLinkedList();
   popTail(); // 100 (head, tail) -> NULL
+  printLinkedList();
   popHead(); // NULL
   printLinkedList();
   return 0;
