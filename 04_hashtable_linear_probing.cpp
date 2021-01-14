0140e5bb0ad5462d689ceafcc7807fc7bc7e8c71
Elvan Selvano <elvanselvano@yahoo.com>
2021-01-15 01:02:25 +0700

    simplify linear probing into its simplest form

--- 04_hashtable_linear_probing.cpp
+++ 04_hashtable_linear_probing.cpp
@@ -1,55 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-const int MAX_ELEMENT = 10; // maksimal elemen dalam hashtable
-const int MAX_LENGTH = 255; // panjang maksimal setiap elemen
-char HASHTABLES[MAX_ELEMENT][MAX_LENGTH]; // bisa menampung elemen sebanyak MAX_ELEMENT dengan tiap elemen memiliki panjang maksimal MAX_LENGTH
+const int MAX_ELEMENT = 5;
+const int MAX_LENGTH = 255;
+char HASHTABLES[MAX_ELEMENT][MAX_LENGTH]; 
+
+int hash(const char *str) { // hash function ascii sum
+  int sum = 0;
+
+  for(int i = 0; str[i] != '\0'; i++) {
+    sum += str[i] - '0';
+  }
+
+  return sum % MAX_ELEMENT; // avoid out of bounds
+}
+
+void linearProbing(int idx, const char *str) {
+  for(int i = (idx+1) % MAX_ELEMENT; i != idx; i = (i+1) % MAX_ELEMENT) {
+    if(!HASHTABLES[i][0]) { // find empty slot from idx+1 to idx-1
+      strcpy(HASHTABLES[i], str);
+      return;
+    }
+  }
+
+  puts("Table is full!");
+}
+
+void insert(const char *str) {
+  int idx = hash(str); // hash the string to get the index
+
+  if(!HASHTABLES[idx][0]) { // if empty
+    strcpy(HASHTABLES[idx], str);
+  } else { // else do linear probing
+    linearProbing(idx, str);
+  }
+}
 
 void view() {
   for(int i = 0; i < MAX_ELEMENT; i++) {
     printf("%d -> %s\n", i, HASHTABLES[i]);
-  }
-}
-
-int hash(const char *str) {
-  // hash function ascii sum
-  int sum = 0;
-  for(int i = 0; str[i] != '\0'; i++) {
-    sum += str[i] - '0'; // sum ditambahkan ascii karakter sekarang
-  }
-
-  return sum % MAX_ELEMENT; // dimodulo agar tidak out of bounds
-}
-
-int linearProbing(int index) {
-  // loop dari index+1 karena index sudah full
-  // untuk tiap iterasi, i = (i+1) % MAX ELEMENT
-  // untuk memastikan i tidak melewati MAX_ELEMENT
-  // pas sebelum dia nyentuh MAX_ELEMENT,
-  // i akan menjadi 0 karena modulo
-  // kemudian i akan loop dari 0 sampai index-1
-
-  for(int i = index+1; i != index; i = (i+1) % MAX_ELEMENT) {
-    if(HASHTABLES[i][0] == '\0') { // jika table di index i kosong
-      return i; // return index kosongnya
-    }
-  }
-
-  return -1; // tidak ketemu index kosong
-}
-
-void insert(const char *str) {
-  int index = hash(str); // dapatkan index melalui hash function
-
-  if(HASHTABLES[index][0] == '\0') { // jika index masih kosong
-    strcpy(HASHTABLES[index], str); // masukkan string di hashtable
-  } else { // jika index sudah terisi
-    index = linearProbing(index); // cari index baru
-    if(index == -1) {
-      puts("Sudah penuh");
-      return;
-    }
-    strcpy(HASHTABLES[index], str); // masukkan string di index baru
   }
 }
 
@@ -59,8 +48,7 @@ int main() {
   insert("kesya");
   insert("nich");
   insert("valencia");
-  insert("laffpai");
-  insert("kevice");
+  // insert("laffpai");
   view();
   return 0;
 }
