9775d20467165bd0cfe2e2b85b8687e434280a4a
Limitleash <71624708+Limitleash@users.noreply.github.com>
2021-01-18 22:29:27 +0700

    Bantu perbaiki bagian inOrderPredecessor

--- 07_binary_search_tree.cpp
+++ 07_binary_search_tree.cpp
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 struct Node {
-  int value;
-  Node *left, *right; // left and right child
+    int value;
+    Node *left, *right; // left and right child
 };
 
 Node *createNode(int value) {
-  Node *temp = (Node*)malloc(sizeof(Node));
-  temp->value = value;
-  temp->left = temp->right = NULL;
-  return temp;
+    Node *temp = (Node*)malloc(sizeof(Node));
+    temp->value = value;
+    temp->left = temp->right = NULL;
+    return temp;
 }
 
 //                  15
@@ -26,124 +26,124 @@ Node *createNode(int value) {
 // 15->left->right->right = 13
 
 Node *insertNode(Node *root, int value) {
-  if(!root) { // empty tree
-    return createNode(value);
-  } else if(value < root->value) { // enter left subtree
-    root->left = insertNode(root->left, value);
-  } else if(value > root->value) { // enter right subtree
-    root->right = insertNode(root->right, value);
-  }
+    if(!root) { // empty tree
+        return createNode(value);
+    } else if(value < root->value) { // enter left subtree
+        root->left = insertNode(root->left, value);
+    } else if(value > root->value) { // enter right subtree
+        root->right = insertNode(root->right, value);
+    }
 
-  return root;
+    return root;
 }
 
 Node *inOrderSucessor(Node *root) { // minimum value in right subtree
-  Node *curr = root->right; // root of right subtree
+    Node *curr = root->right; // root of right subtree
 
-  while(curr && curr->left) { 
-    curr = curr->left; // find minimum value by traversing left
-  }
+    while(curr != NULL && curr->left != NULL) { 
+        curr = curr->left; // find minimum value by traversing left
+    }
 
-  return curr;
+    return curr;
 }
 
 Node *inOrderPredecessor(Node *root) { // maximum value in left subtree
-  Node *curr = root->right; // root of left subtree
+    Node *curr = root->left; // root of left subtree
 
-  while(curr && curr->right) { 
-    curr = curr->right; // find maximum value by traversing right
-  }
+    while(curr && curr->right) { 
+        curr = curr->right; // find maximum value by traversing right
+    }
 
-  return curr;
+    return curr;
 }
 
 Node *deleteNode(Node *root, int value) {
-  if(!root) { // empty tree
-    return root;
-  } else if(value < root->value) { // enter left subtree
-    root->left = deleteNode(root->left, value);
-  } else if(value > root->value) { // enter right subtree
-    root->right = deleteNode(root->right, value);
-  } else {
+    if(!root) { // empty tree
+        return root;
+    } else if(value < root->value) { // enter left subtree
+        root->left = deleteNode(root->left, value);
+    } else if(value > root->value) { // enter right subtree
+        root->right = deleteNode(root->right, value);
+    } else {
 
-    if(!root->left || !root->right) { // 0 and 1 child
-      // if left child is null, new root is right child
-      // if right child is null, new root is left child
-      Node *newRoot = root->left ? root->left : root->right;
-      free(root); // remove old root
-      root = NULL;
-      return newRoot;
+        if(root->left == NULL || root->right == NULL) { // 0 and 1 child
+            // if left child is null, new root is right child
+            // if right child is null, new root is left child
+            Node *newRoot = root->left != NULL ? root->left : root->right;
+            free(root); // remove old root
+            root = NULL;
+            return newRoot;
+        }
+
+        // 2 children using inOrderSucessor
+        // Node *newRoot = inOrderSucessor(root);
+        // root->value = newRoot->value; // copy value
+        // root->right = deleteNode(root->right, newRoot->value); // remove the inorder successor
+
+        // 2 children using inOrderPredecessor
+        Node *newRoot = inOrderPredecessor(root);
+        root->value = newRoot->value; // copy value
+        root->left = deleteNode(root->left, newRoot->value); // remove the inorder predecessor
     }
 
-    // 2 children using inOrderSucessor
-    Node *newRoot = inOrderSucessor(root);
-    root->value = newRoot->value; // copy value
-    root->right = deleteNode(root->right, newRoot->value); // remove the inorder successor
-
-    // 2 children using inOrderPredecessor
-    // Node *newRoot = inOrderPredecessor(root);
-    // root->value = newRoot->value; // copy value
-    // root->right = deleteNode(root->left, newRoot->value); // remove the inorder predecessor
-  }
-
-  return root;
+    return root;
 }
 
 bool searchNode(Node *root, int key) {
-  if(root) {
-    if(root->value == key) {
-      return true;
-    } else if(key < root->value) {
-      return searchNode(root->left, key); 
-    } else if(key > root->value) {
-      return searchNode(root->right, key);
+    if(root != NULL) {
+        if(root->value == key) {
+        return true;
+        } else if(key < root->value) {
+        return searchNode(root->left, key); 
+        } else if(key > root->value) {
+        return searchNode(root->right, key);
+        }
     }
-  }
 
-  return false;
+    return false;
 }
 
 void preOrder(Node *root) {
-  if(root) {
-    printf("%d ", root->value);
-    preOrder(root->left);
-    preOrder(root->right);
-  }
+    if(root) {
+        printf("%d ", root->value);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
 }
 
 void inOrder(Node *root) {
-  if(root) {
-    inOrder(root->left);
-    printf("%d ", root->value);
-    inOrder(root->right);
-  }
+    if(root) {
+        inOrder(root->left);
+        printf("%d ", root->value);
+        inOrder(root->right);
+    }
 }
 
 void postOrder(Node *root) {
-  if(root) {
-    postOrder(root->left);
-    postOrder(root->right);
-    printf("%d ", root->value);
-  }
+    if(root) {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->value);
+    }
 }
 
 int main() {
-  Node *baseRoot = NULL;
-  int toInsert[] = {10, 11, 12, 5, 3, 2, 8, 7, 4, 1, 9, 25, 6, 30, 31};
+    Node *baseRoot = NULL;
+    int toInsert[] = {10, 11, 12, 5, 3, 2, 8, 7, 4, 1, 9, 25, 6, 30, 31};
 
-  for(int i = 0; i < 15; i++) {
-    baseRoot = insertNode(baseRoot, toInsert[i]);
-    printf("Added %d to the tree.\n", toInsert[i]);
-    inOrder(baseRoot);
-    puts("");
-  }
+    for(int i = 0; i < 15; i++) {
+        baseRoot = insertNode(baseRoot, toInsert[i]);
+        printf("Added %d to the tree.\n", toInsert[i]);
+        inOrder(baseRoot);
+        puts("");
+    }
 
-  for(int i = 0; i < 15; i++) {
-    baseRoot = deleteNode(baseRoot, toInsert[i]);
-    printf("Removed %d from the tree.\n", toInsert[i]);
-    inOrder(baseRoot);
-    puts("");
-  }
+    for(int i = 0; i < 15; i++) {
+        baseRoot = deleteNode(baseRoot, toInsert[i]);
+        printf("Removed %d from the tree.\n", toInsert[i]);
+        inOrder(baseRoot);
+        puts("");
+    }
 
-  return 0;
+    return 0;
 }
