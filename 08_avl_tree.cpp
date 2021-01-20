3ff874deeaf61f59677553dfafd07e9641b77248
Elvan Selvano <elvanselvano@yahoo.com>
2021-01-20 23:39:05 +0700

    add balance factor for visualization

--- 08_avl_tree.cpp
+++ 08_avl_tree.cpp
@@ -1,75 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Reading material: https://medium.com/@elvanselvano/avl-tree-perfectly-balanced-as-all-things-should-be-ca15a6d59ac5
 struct Node {
-  int value, height;
+  int value, height, bf;
   Node *left, *right;
 };
 
-int max(int a, int b) { // find maximum between 2 numbers
+int max(int a, int b) { // maximum between 2 numbers
   return (a > b) ? a : b;
 }
 
-int getHeight(Node *root) { // find height of a root
+int getHeight(Node *root) { // height of a node
   return (root) ? root->height : 0;
 }
 
-// BF = height of left child - height of right child
-int getBalanceFactor(Node *root) { 
+int getBalanceFactor(Node *root) {  // height of left child - height of right child
   return (root) ? getHeight(root->left) - getHeight(root->right) : 0;
 }
 
-// find new height of a root during insertion or deletion
-// height = max between left child or right child, plus 1
-int computeNewHeight(Node *root) {
+int computeNewHeight(Node *root) { // new height after insertion or deletion
   return max(getHeight(root->left), getHeight(root->right)) + 1;
-}
-
-// Rotation: https://drive.google.com/drive/folders/1_dRl8kpv5TSJfLvUaGAuoyij0ZreyLSu?usp=sharing
-Node *leftRotate(Node *x) {
-  Node *y = x->right;
-  Node *z = y->left;
-  y->left = x;
-  x->right = z;
-  x->height = computeNewHeight(x);
-  y->height = computeNewHeight(y);
-  return y;
-}
-
-Node *rightRotate(Node *x) {
-  Node *y = x->left;
-  Node *z = y->right;
-  y->right = x;
-  x->left = z;
-  x->height = computeNewHeight(x);
-  y->height = computeNewHeight(y);
-  return y;
 }
 
 Node *createNode(int value) {
   Node *newNode = (Node*)malloc(sizeof(Node));
   newNode->value = value;
   newNode->height = 1;
+  newNode->bf = 0;
   newNode->left = newNode->right = NULL;
   return newNode;
 }
 
-Node *rotation(Node *root) {
-  root->height = computeNewHeight(root); // get new height after insertion/deletion
-  int BFRoot = getBalanceFactor(root); // balance factor root
-  int BFRight = getBalanceFactor(root->right); // balance factor right child
-  int BFLeft = getBalanceFactor(root->left); // balance factor left child
+Node *updateNode(Node *root) {
+  root->height = computeNewHeight(root);
+  root->bf = getBalanceFactor(root);
+  return root;
+}
 
-  if(BFRoot > 1 && BFLeft >= 0) { // BFRoot > 1 = left child unbalanced
+// Rotation: https://drive.google.com/drive/folders/1_dRl8kpv5TSJfLvUaGAuoyij0ZreyLSu?usp=sharing
+Node *leftRotate(Node *root) {
+  Node *pivot = root->right;
+  Node *temp = pivot->left;
+  pivot->left = root;
+  root->right = temp;
+  root = updateNode(root);
+  pivot = updateNode(pivot);
+  return pivot;
+}
+
+Node *rightRotate(Node *root) {
+  Node *pivot = root->left;
+  Node *temp = pivot->right;
+  pivot->right = root;
+  root->left = temp;
+  root = updateNode(root);
+  pivot = updateNode(pivot);
+  return pivot;
+}
+
+Node *rotation(Node *root) {
+  if(root->bf > 1 && root->left->bf >= 0) { // bf > 1 = left child unbalanced
     return rightRotate(root);
-  } else if(BFRoot > 1 && BFLeft < 0) { // BFRoot < -1 = right child unbalanced
-    root->left = leftRotate(root->left);
+  } else if(root->bf > 1 && root->left->bf < 0) {
+    root->left = leftRotate(root->left); // double rotation
     return rightRotate(root);
-  } else if(BFRoot < -1 && BFRight <= 0) {
+  } else if(root->bf < -1 && root->right->bf <= 0) { // bf < -1 = right child unbalanced
     return leftRotate(root);
-  } else if(BFRoot < -1 && BFRight > 0) {
-    root->right = rightRotate(root->right);
+  } else if(root->bf < - 1 && root->right->bf > 0) {
+    root->right = rightRotate(root->right); // double rotation
     return leftRotate(root);
   }
 
@@ -85,7 +83,7 @@ Node *insertNode(Node *root, int value) {
     root->right = insertNode(root->right, value);
   }
 
-  return rotation(root);
+  return rotation(updateNode(root));
 }
 
 Node *predecessor(Node *root) {
@@ -108,23 +106,24 @@ Node *deleteNode(Node *root, int value) {
   } else {
     if(!root->left || !root->right) {
       Node *newRoot = root->left ? root->left : root->right;
+      root->left = root->right = NULL;
       free(root);
       root = NULL;
       return newRoot;
     }
       
-    Node *temp = predecessor(root);
-    root->value = temp->value;
-    root->left = deleteNode(root->left, temp->value);
+    Node *inOrderPredecessor = predecessor(root);
+    root->value = inOrderPredecessor->value;
+    root->left = deleteNode(root->left, inOrderPredecessor->value);
   }
 
-  return (root) ? rotation(root) : root;
+  return rotation(updateNode(root));
 }
 
 void inOrder(Node *root) {
   if(root) {
     inOrder(root->left);
-    printf("Height: %d, Value %d\n", root->height, root->value);
+    printf("| %-3d | %4d   | %7d        |\n", root->value, root->height, root->bf);
     inOrder(root->right);
   }
 }
@@ -136,13 +135,15 @@ int main() {
   for(int i = 0; i < 9; i++) {
     baseRoot = insertNode(baseRoot, toInsert[i]);
     printf("\nAdded %d to the tree.\n", toInsert[i]);
-    inOrder(baseRoot);
+    printf("| %-3s | %-5s | %-14s |\n", "Key", "Height", "Balance Factor");
+    inOrder(baseRoot); getchar();
   }
 
   for(int i = 0; i < 9; i++) {
     baseRoot = deleteNode(baseRoot, toInsert[i]);
     printf("\nRemoved %d from the tree.\n", toInsert[i]);
-    inOrder(baseRoot);
+    printf("| %-3s | %-5s | %-14s |\n", "Key", "Height", "Balance Factor");
+    inOrder(baseRoot); getchar();
   }
   
   return 0;
